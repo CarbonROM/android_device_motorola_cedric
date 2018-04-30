0ca716fb8c85107cdfcec3a9aaaf70ef252c21c5
Henrique Silva <jhenrique09.mcz@hotmail.com>
2018-04-30 20:49:54 -0300

    cedric: LineageActions -> MotoActions

--- device.mk
+++ device.mk
@@ -117,9 +117,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     CarrierConfig
 
-# LineageActions
+# MotoActions
 PRODUCT_PACKAGES += \
-    LineageActions
+    MotoActions
 
 # Display
 PRODUCT_PACKAGES += \
